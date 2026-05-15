4d9a3ca8b0419d1b25732c9d85ebd282f89adda3
Jeevan-419 <jeevannj419@gmail.com>
2026-05-16 02:18:06 +0530

    Fix Vercel catch-all routing

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// This file acts as the bridge for Vercel Serverless Functions.
-// Vercel automatically deploys any file inside the 'api/' folder as a serverless function.
-const app = require('../backend/server.js');
-
-module.exports = app;
